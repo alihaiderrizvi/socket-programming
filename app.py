af3e68ac196213099642204f4ccc942ab349a261
Ali Haider Rizvi <45711004+alihaiderrizvi@users.noreply.github.com>
2021-11-15 14:06:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask #, render_template, session, copy_current_request_contex
 # import socket 
 # import threading
 from flask_sock import Sock
-from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+# from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
 
 
 app = Flask(__name__)
@@ -43,9 +43,9 @@ sock = Sock(app)
 
 @sock.route('/')
 def bot_chat(ws):
-    print('making model . . .')
-    tokenizer = BlenderbotTokenizer.from_pretrained("facebook/blenderbot-400M-distill")
-    model = BlenderbotForConditionalGeneration.from_pretrained("facebook/blenderbot-400M-distill")
+    #print('making model . . .')
+    #tokenizer = BlenderbotTokenizer.from_pretrained("facebook/blenderbot-400M-distill")
+    #model = BlenderbotForConditionalGeneration.from_pretrained("facebook/blenderbot-400M-distill")
     print('ready to chat!')
     flag = True
     while flag:
@@ -54,11 +54,12 @@ def bot_chat(ws):
         if message == 'disconnect':
             flag = False
             break
-        inputs = tokenizer(message, return_tensors="pt")
-        res = model.generate(**inputs)
-        reply = tokenizer.decode(res[0])
-        reply = reply.replace('<s>', '')
-        reply = reply.replace('</s>', '')
+#         inputs = tokenizer(message, return_tensors="pt")
+#         res = model.generate(**inputs)
+#         reply = tokenizer.decode(res[0])
+#         reply = reply.replace('<s>', '')
+#         reply = reply.replace('</s>', '')
+        reply = 'hello'
         print('bot:', reply)
         ws.send(reply)
 
